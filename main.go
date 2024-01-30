86d541697e2de2cf98a0a376d8e3c0e93a7aebcf
Ha0-Y <yuanhao.it@foxmail.com>
2024-01-30 15:30:47 +0800

    goroutinepool

--- main.go
+++ main.go
@@ -1,26 +1,75 @@
 package main
 
 import (
-	`fmt`
-	`net`
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"sort"
+	"time"
 )
 
-func main() {
-	var addr string
-	var err error
-	var conn net.Conn
-	IP := "127.0.0.1"
-	for port := 22; port < 1024; port++ {
-		addr = fmt.Sprintf("%s:%d", IP, port)
-		conn, err = net.Dial("tcp", addr)
+// chan <- 运算会阻塞
+
+var (
+	hostname  string
+	startPort int
+	endPort   int
+)
+
+func worker(ports, result chan int) {
+	for port := range ports {
+		addr := fmt.Sprintf("%s:%d", hostname, port)
+		conn, err := net.DialTimeout("tcp", addr, 2*time.Second)
 		if err != nil {
-			fmt.Printf("%d close\n", port)
+			result <- 0
 			continue
 		}
-		fmt.Printf("%d open\n", port)
 		err = conn.Close()
 		if err != nil {
-			panic(err)
+			log.Fatalf("Error closing connection: %v", err)
 		}
+		result <- port
+	}
+}
+
+func main() {
+	flag.StringVar(&hostname, "h", "", "hostname")
+	flag.IntVar(&startPort, "start-port", 21, "scanning start port")
+	flag.IntVar(&endPort, "end-port", 65535, "scanning end port")
+	flag.Parse()
+	//hostname = "127.0.0.1"
+	//startPort = 21
+	//endPort = 1024
+
+	fmt.Println("Start scanning...")
+	ports := make(chan int, 100)
+	res := make(chan int)
+	var openPorts []int
+
+	// defer 语句：在函数栈帧返回前插入
+	defer close(ports)
+	defer close(res)
+
+	for i := 0; i < cap(ports); i++ {
+		go worker(ports, res)
+	}
+
+	go func() {
+		for port := startPort; port <= endPort; port++ {
+			ports <- port
+		}
+	}()
+
+	for i := startPort; i <= endPort; i++ {
+		p := <-res
+		if p != 0 {
+			openPorts = append(openPorts, p)
+		}
+	}
+
+	sort.Ints(openPorts)
+	for _, v := range openPorts {
+		fmt.Printf("[+] %d open\n", v)
 	}
 }
