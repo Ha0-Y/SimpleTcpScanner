43758d023f835947f255248582ab3c2770fae8c4
Ha0-Y <yuanhao.it@foxmail.com>
2024-01-30 12:05:59 +0800

    goroutines

--- main.go
+++ main.go
@@ -1,26 +1,32 @@
 package main
 
 import (
-	`fmt`
-	`net`
+	"fmt"
+	"net"
+	"sync"
 )
+
+var wg sync.WaitGroup
 
 func main() {
 	var addr string
-	var err error
-	var conn net.Conn
 	IP := "127.0.0.1"
 	for port := 22; port < 1024; port++ {
-		addr = fmt.Sprintf("%s:%d", IP, port)
-		conn, err = net.Dial("tcp", addr)
-		if err != nil {
-			fmt.Printf("%d close\n", port)
-			continue
-		}
-		fmt.Printf("%d open\n", port)
-		err = conn.Close()
-		if err != nil {
-			panic(err)
-		}
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			addr = fmt.Sprintf("%s:%d", IP, i)
+			conn, err := net.Dial("tcp", addr)
+			if err != nil {
+				fmt.Printf("%d close \n", i)
+				return
+			}
+			fmt.Printf("%d open \n", i)
+			err = conn.Close()
+			if err != nil {
+				panic(err)
+			}
+		}(port)
 	}
+	wg.Wait()
 }
